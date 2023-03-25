ca10018a90d4350dd271513d616c4cb2325a0a3b
Stargik <a.stargik@gmail.com>
2023-03-25 20:12:34 +0200

    Modified SpeakerRepository

--- DAL/Interfaces/ISpeakerRepository.cs
+++ DAL/Interfaces/ISpeakerRepository.cs
@@ -1,8 +1,12 @@
 ﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
     public interface ISpeakerRepository : IRepository<Speaker>
     {
+        Task<Speaker> GetByIdWithDetailsAsync(int id);
+        Task<IEnumerable<Speaker>> GetAllWithDetailsAsync();
     }
 }
--- DAL/Repositories/SpeakerRepository.cs
+++ DAL/Repositories/SpeakerRepository.cs
@@ -1,6 +1,9 @@
 ﻿using DAL.Data;
 using DAL.Entities;
 using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
@@ -8,6 +11,16 @@ namespace DAL.Repositories
     {
         public SpeakerRepository(EventCatalogDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<Speaker>> GetAllWithDetailsAsync()
+        {
+            return await context.Speakers.Include(speaker => speaker.Events).ToListAsync();
+        }
+
+        public async Task<Speaker> GetByIdWithDetailsAsync(int id)
+        {
+            return await context.Speakers.Include(speaker => speaker.Events).SingleOrDefaultAsync(speaker => speaker.Id == id);
         }
     }
 }
