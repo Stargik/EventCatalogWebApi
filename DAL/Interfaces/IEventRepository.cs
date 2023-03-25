5d729042767c36d36164c550726d3a52a4394eff
Stargik <a.stargik@gmail.com>
2023-03-25 20:09:08 +0200

    Modified IEventRepository

--- DAL/Interfaces/IEventRepository.cs
+++ DAL/Interfaces/IEventRepository.cs
@@ -1,8 +1,13 @@
 ﻿using DAL.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
     public interface IEventRepository : IRepository<Event>
     {
+        Task<Event> GetByIdWithDetailsAsync(int id);
+        Task<IEnumerable<Event>> GetAllWithDetailsAsync();
     }
 }
--- DAL/Repositories/EventRepository.cs
+++ DAL/Repositories/EventRepository.cs
@@ -1,6 +1,9 @@
 ﻿using DAL.Data;
 using DAL.Entities;
 using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
@@ -8,6 +11,16 @@ namespace DAL.Repositories
     {
         public EventRepository(EventCatalogDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<Event>> GetAllWithDetailsAsync()
+        {
+            return await context.Events.Include(e => e.Speaker).Include(e => e.Participants).Include(e => e.EventFormat).Include(e => e.EventSubjectCategory).ToListAsync();
+        }
+
+        public async Task<Event> GetByIdWithDetailsAsync(int id)
+        {
+            return await context.Events.Include(e => e.Speaker).Include(e => e.Participants).Include(e => e.EventFormat).Include(e => e.EventSubjectCategory).SingleOrDefaultAsync(e => e.Id == id);
         }
     }
 }
